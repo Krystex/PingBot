8dff93b83e36ebbc45a3cbc830120c6e6a72a9af
Krystex <5840423+Krystex@users.noreply.github.com>
2022-08-01 19:31:40 +0200

    Added simple scheduler

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+const INTERVAL = 1000  // Execute every second
+
+const schedule = (func) => {
+  const exec = () => {
+    ms = new Date().getTime() % INTERVAL
+    setTimeout(_ => {
+      exec()
+      func()
+    }, INTERVAL - ms)
+  }
+  exec()
+}
+
+schedule(_ => {
+  console.log(new Date().getTime())
+})
