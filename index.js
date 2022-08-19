cb903d68a77ab08ca6d4c13d23b6a446bccdbb74
Krystex <5840423+Krystex@users.noreply.github.com>
2022-08-19 12:57:48 +0200

    ping duration now gets parsed

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const { exec } = require("child_process")
 
 const INTERVAL = 1000  // Execute every second
 
-const schedule = (func) => {
+const schedule = (interval, func) => {
   const exec = () => {
     // Calculate difference to next executing
-    diff = INTERVAL - (new Date().getTime() % INTERVAL)
+    diff = interval - (new Date().getTime() % interval)
     setTimeout(_ => {
       // Recursively plan next executing
       exec()
       // Round time (because we don't care about milliseconds difference) and execute actual function
-      const time = Math.round(new Date().getTime() / INTERVAL) * INTERVAL
+      const time = Math.round(new Date().getTime() / interval) * interval
       func(time)
     }, diff)
   }
   exec()
 }
 
+const timeParser = (lines) => {
+  // Return if not enough lines are supplied
+  if (lines.length < 2) return NaN
+  // Execute regex
+  const reg = /time=(\d*\.\d*)/.exec(lines[1])
+  // if regex failed, return NaN
+  if ((reg === null) || (reg.length < 2)) return NaN
+  else return reg[1]
+}
+
 // Main entrypoint
-schedule(time => {
+schedule(INTERVAL, timestamp => {
   exec("ping -c1 google.com", (error, stdout, stderr) => {
-    console.error(error)
-    console.log(stdout)
-    console.error(stderr)
+    // console.error(error)
+    const lines = stdout.split("\n")
+    const time = timeParser(lines)
+    console.log(time)
   })
 })
